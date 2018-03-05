bfbd1d1c134d024b5641c3c30156359dbb114719
Alex Fernandes Figueiredo <alex.figueiredo@nutes.uepb.edu.br>
2018-03-05 19:34:08 -0300

    Implementing http get request on Class 3 of Be Mean - NodeJS

---
+++ Aula3/http-request.js
@@ -0,0 +1,27 @@
+const http = require('http');
+
+const requestOptions = {
+    host:'www.google.com',
+    path:'/'
+};
+
+function requestCallback(res){
+    console.log('Request result - STATUS: ' + res.statusCode);
+    console.log('Request result - HEADERS: ' + JSON.stringify(res.headers));
+
+    let data = '';
+    
+    res.setEncoding('utf8');
+    res.on('data',function(newData){
+        data += newData;
+    });    
+    res.on('end',function(){
+        console.log('Request result - DATA:', data);
+    });
+};
+
+const req = http.request(requestOptions, requestCallback);
+req.on('error', function(e){
+    console.log('Request error: ' + e);
+});
+req.end();
